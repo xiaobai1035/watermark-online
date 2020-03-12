b4fb08559565e90808a27c4fe171af50a10747aa
xiaobai1035 <2415979887@qq.com>
2020-03-12 19:30:15 +0800

    Create constant.js

---
+++ watermark/js/constant.js
@@ -0,0 +1,49 @@
+// 颜色选择器颜色列表
+const COLOR_LIST = [
+    {"primary": "#E91E63"},
+    {"primary_dark": "#C2185B"},
+    {"primary_light": "#F8BBD0"},
+    {"accent": "#CDDC39"},
+    {"primary_text": "#212121"},
+    {"secondary_text": "#727272"},
+    {"divider": "#B6B6B6"}
+]
+// 字体列表
+,FONT_FAMILY_LIST = {
+    "SimSun": "宋体",
+    "SimHei": "黑体",
+    "Yahei": "微软雅黑",
+    "JhengHei": "微软正黑体",
+    "KaiTi": "楷体",
+    "NSimSun": "新宋体",
+    "FangSong": "仿宋"
+}
+// 字号列表
+,FONT_SIZE = {
+    "smallsix": ["小六", 8],
+    "normalsix": ["六号", 10],
+    "smallfive": ["小五", 12],
+    "normalfive": ["五号", 14],
+    "smallfour": ["小四", 16],
+    "normalfour": ["四号", 18],
+    "smallthree": ["小三", 20],
+    "normalthree": ["三号", 21],
+    "smalltwo": ["小二", 24],
+    "normaltwo": ["二号", 29],
+    "smallone": ["小一", 32],
+    "normalone": ["一号", 34],
+    "smallzero": ["小初", 48],
+    "normalzero": ["初号", 56]
+}
+// 字体粗细
+,FONT_WEIGHT = {
+    "100": 100,
+    "200": 200,
+    "300": 300,
+    "400": 400,
+    "500": 500,
+    "600": 600,
+    "700": 700,
+    "800": 800,
+    "900": 900
+}
